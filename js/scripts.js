cd3f3e3dec46faf71176263c27f878792c54f7b5
robinescaffre <robin.escaffre@edu.esiee.fr>
2024-08-19 10:36:00 +0200

    v0.11 : test carte 2

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,7 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+<>
+    <script type="text/javascript" src="https://mrhuran.github.io/mapdata.js"></script>
+    <script type="text/javascript" src="https://mrhuran.github.io/worldmap.js"></script>
+</>
